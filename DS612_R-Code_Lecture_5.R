7667406b1a676c72b64fef33fadc821fc4e48ec8
NormanLo4319 <lokman@mail.sfsu.edu>
2019-12-05 22:13:13 -0800

    Updated on 12/5/2019

--- DS612_R-Code_Lecture_5.R
+++ DS612_R-Code_Lecture_5.R
@@ -38,43 +38,58 @@ loocv = function(fit){
 loocv(glm.fit)
 
 # Using a for loop to check the the k-fold cross validation comparison
+# Try to use 10 cross validation outcomes
 cv.error <- rep(0,10)
+# Degree of polynomial from 1 to 10
 degree <- 1:10
 for(d in degree){
-  glm.fit=glm(mpg~poly(horsepower,d), data=Auto)
-  cv.error[d]=loocv(glm.fit)
+  glm.fit <- glm(mpg~poly(horsepower,d), data=Auto)
+  cv.error[d] <- loocv(glm.fit)
 }
 
-# Plot the results and compare different degree for cross validation
+# Plot the cv error and compare different degrees for cross validation
 plot(degree,cv.error,type="b")
 
-## 10-fold CV
 
-cv.error10=rep(0,5)
+# Now, let's try to use the 10-fold cross validation
+# Try to use 5 cross validation outcomes with degree of polynomial from 1 to 10
+cv.error10 <- rep(0,5)
 for(d in degree){
-  glm.fit=glm(mpg~poly(horsepower,d), data=Auto)
-  cv.error10[d]=cv.glm(Auto,glm.fit,K=10)$delta[1]
+  glm.fit <- glm(mpg~poly(horsepower, d), data=Auto)
+  cv.error10[d] <- cv.glm(Auto,glm.fit, K=10)$delta[1]
 }
-lines(degree,cv.error10,type="b",col="red")
 
-## Bootstrap
-## Minimum risk investment - Section 5.2
+# Plot the line that represents the degree and cv error
+lines(degree, cv.error10, type="b", col="red")
 
-## What is the standard error of alpha?
+# Bootstrap
+# Minimum Risk Investment - Section 5.2
+
+# We are going to use 'Portfolio' data set
 ?Portfolio
 
-alpha.fn=function(data, index){
+# What is the standard error of alpha?
+# We are going to create an alpha function for the analysis
+alpha.fn = function(data, index){
   X = data$X[index]
   Y = data$Y[index]
   return((var(Y)-cov(X,Y))/(var(X)+var(Y)-2*cov(X,Y)))
 }
 
+# Use the alpha.fn() function to calculate the standard error for alpha
 alpha.fn(Portfolio,1:100)
+
+# Set a random seed number
 set.seed(1)
+
+# Use the alpha.fn() function again on the 100 sample in the data set
 alpha.fn(Portfolio,sample(1:100,100,replace=TRUE))
 
-boot.out=boot(Portfolio,alpha.fn,R=1000)
+# Use boot() function for repeated measures by 1000 times
+boot.out <- boot(Portfolio,alpha.fn,R=1000)
 boot.out
+
+# Plotting the bootstrap output
 plot(boot.out)
 
 ?with
