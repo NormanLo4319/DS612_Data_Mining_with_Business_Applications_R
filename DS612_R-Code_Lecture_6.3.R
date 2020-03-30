f2c1b59c8c9f4b7a4ec3aef2f9f7183aff876707
NormanLo4319 <lokman@mail.sfsu.edu>
2020-03-30 14:57:05 -0700

    Updated on 3/30/2020

--- DS612_R-Code_Lecture_6.3.R
+++ DS612_R-Code_Lecture_6.3.R
@@ -20,7 +20,8 @@ attach(Carseats)
 # let's explore the dataset
 head(Carseats)
 # 400 observations 11 variables
-dim(Carseats) 
+dim(Carseats)
+# print summary for catagorical variable "ShelveLoc"
 summary(ShelveLoc)
 
 # We want to predict Sales based on other variables 
@@ -28,7 +29,7 @@ summary(ShelveLoc)
 # Let's randomly divide our data into two groups - test and training
 
 # you can set.seed to any number
-set.seed(2) 
+set.seed(1) 
 NumberofObservations = nrow(Carseats)
 NumberofObservations # that is 400
 SplitofTrainTest = 0.5 # let's split the data 50/50
@@ -42,6 +43,8 @@ Testing_outcome = Sales[test] # The is the our test outcomes
 # Now it is the TREE TIME :)
 # You need to use training data
 Tree_Model = tree(Sales~.,trainingData)
+summary(Tree_Model)
+
 # note there is no text on the tree
 plot(Tree_Model)
 # Use pretty=0 only when you have categorical variables
@@ -53,14 +56,14 @@ Tree_pred = predict(Tree_Model,testingData)
 names(Tree_pred)
 # Let's compute the error
 mean((Tree_pred - Testing_outcome)^2) 
-#4.844991 You may find another number based on your random seed
+#4.922039 You may find another number based on your random seed
 
 
 ##### PRUNE THE TREE
 
 # May be we can do better with pruning
 # you can set it to any number!
-set.seed(3) 
+set.seed(1) 
 # Five fold cross-validation
 cv_tree = cv.tree(Tree_Model,K=5)
 # Size is the size of the tree, Dev is cross-validation Error
@@ -70,11 +73,12 @@ plot(cv_tree$size,cv_tree$dev,type="b")
 
 
 # Now let's prune our tree - that is the gardening time!
-pruned_Model = prune.tree(Tree_Model, best=12) #We pruned our model based on best size we found
+pruned_Model = prune.tree(Tree_Model, best=10) #We pruned our model based on best size we found
 # in cross-validation - remember it was 12 (you might have found another number)
 plot(pruned_Model)
 text(pruned_Model,pretty=0)
 
+# Let's compute the error
 Tree_pred_new = predict(pruned_Model,testingData)
-mean((Tree_pred_new - Testing_outcome)^2) #error decreased to 4.667
+mean((Tree_pred_new - Testing_outcome)^2) #error decreased to 4.918134
 
