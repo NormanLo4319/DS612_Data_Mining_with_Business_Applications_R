bc703355855668918e709e4cd5b73d7425ff1dec
NormanLo4319 <lokman@mail.sfsu.edu>
2020-04-14 10:29:50 -0700

    Updated on 4/14/2020

--- DS612_R-Code_Lecture_7.1.R
+++ DS612_R-Code_Lecture_7.1.R
@@ -11,39 +11,39 @@ attach(Wage)
 
 # In this example, we are tyring to predict wages using age from the dataset
 # First fit the model using the basic command for polynomial regression
-fit.Poly.Basic = lm(wage~age+I(age^2)+I(age^3)+I(age^4),data=Wage)
+fit.Poly.Basic <- lm(wage~age+I(age^2)+I(age^3)+I(age^4),data=Wage)
 summary(fit.Poly.Basic)
 
 # We can also use the poly() function to fit the same model
-fit.Poly =lm(wage~poly(age,4,raw=TRUE),data=Wage)
+fit.Poly <- lm(wage~poly(age,4, raw=TRUE),data=Wage)
 summary(fit.Poly)
 
 # Note that if we do not define raw=TRUE, the model will still have the same fitted values
 # However, the coefficients for each term cannot be interpreted as usual
 # In the orthogonal polynomial form, only the p-value can be interpreted
-fit.Poly.Orth =lm(wage~poly(age,4),data=Wage)
+fit.Poly.Orth <- lm(wage~poly(age,4), data=Wage)
 summary(fit.Poly.Orth)
 
 # Let's create a grid of valuesfor age for prediction
-agelims = range(age)
-age.grid=seq(agelims[1],to=agelims[2])
-preds = predict(fit.Poly,newdata = list(age=age.grid),se = TRUE)
+agelims <- range(age)
+age.grid <- seq(agelims[1], to=agelims[2])
+preds <- predict(fit.Poly,newdata=list(age=age.grid), se = TRUE)
 names(preds)
-se.bands = cbind(preds$fit+2*preds$se.fit,preds$fit-2*preds$se.fit)
+se.bands <- cbind(preds$fit+2*preds$se.fit, preds$fit-2*preds$se.fit)
 
 
 # Plot the data dn add the fit from the degree-4 polynomial
-plot(age,wage,col="darkgray")
-lines(age.grid,preds$fit,lwd=2,col="blue") #lwd is linewidth
-matlines(age.grid,se.bands,col="blue",lty=2)  #lty shows type of line lty = 2 is used for broken lines
+plot(age, wage, col="darkgray")
+lines(age.grid, preds$fit, lwd=2, col="blue") #lwd is linewidth
+matlines(age.grid, se.bands, col="blue", lty=2)  #lty shows type of line lty = 2 is used for broken lines
 
 
 #### Using Anova in Nested sequence of Models ####
-fita = lm(wage~education,data=Wage)
-fitb = lm(wage~education + age,data=Wage)
-fitc = lm(wage~education + poly(age,2),data=Wage)
-fitd = lm(wage~education + poly(age,3),data=Wage)
-fite = lm(wage~education + poly(age,4),data=Wage)
+fita <- lm(wage~education, data=Wage)
+fitb <- lm(wage~education + age, data=Wage)
+fitc <- lm(wage~education + poly(age,2), data=Wage)
+fitd <- lm(wage~education + poly(age,3), data=Wage)
+fite <- lm(wage~education + poly(age,4), data=Wage)
 
 # The p-value indicates the model with the prior model
 # The p-vlaue is significant means the prior model is not sufficient
@@ -55,27 +55,27 @@ anova(fita,fitb,fitc,fitd,fite)
 # Fitting the logistic regression model on binary response, wage,
 # using wrapper I() to create binary response, over 250 (True) and below or equal 250 (False)
 # on age with polynomial degree 3
-fit.Logistics = glm(I(wage>250)~poly(age,3), data=Wage, family=binomial)
+fit.Logistics <- glm(I(wage>250)~poly(age,3), data=Wage, family=binomial)
 summary(fit.Logistics)
 
 # Predict the response by the age grid created earlier
-preds.Logistics = predict(fit.Logistics, newdata=list(age=age.grid), se=TRUE)
+preds.Logistics <- predict(fit.Logistics, newdata=list(age=age.grid), se=TRUE)
 names(preds.Logistics)
 
 # Create the confidence intervals for the prediction
-se.bands = preds.Logistics$fit+cbind(fit=0, lower=-2*preds.Logistics$se.fit, upper=2*preds.Logistics$se.fit)
+se.bands <- preds.Logistics$fit+cbind(fit=0, lower=-2*preds.Logistics$se.fit, upper=2*preds.Logistics$se.fit)
 se.bands[1:5,]
 
 # Convert logistics to probabilities
-prob.bands = exp(se.bands)/(1+exp(se.bands))
+prob.bands <- exp(se.bands)/(1+exp(se.bands))
 prob.bands[1:5,]
 
 # Plot the predictions with confidence intervals
-matplot(age.grid,prob.bands,col="blue",lwd=c(2,1,1),lty=c(1,2,2),type="l")
+matplot(age.grid, prob.bands, col="blue", lwd=c(2,1,1), lty=c(1,2,2), type="l")
 
 #### Fitting step functions ####
 # In order to fit a step function, we need to use the cut() function
 ?cut
 table(cut(age,4))
-fit.Step = lm(age~cut(age,4),data=Wage)
+fit.Step <- lm(age~cut(age,4), data=Wage)
 summary(fit.Step)
