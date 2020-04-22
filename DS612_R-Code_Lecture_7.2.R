ead4f0489ea498a6dcc01a2fd7511ae9f0e7042c
NormanLo4319 <lokman@mail.sfsu.edu>
2020-04-21 17:44:24 -0700

    Updated on 4/21/2020

--- DS612_R-Code_Lecture_7.2.R
+++ DS612_R-Code_Lecture_7.2.R
@@ -3,6 +3,8 @@
 #--------Moving Beyond Linearity--------#
 #---------------------------------------#
 
+rm(list=ls(all=TRUE))
+
 #### Splines ####
 library(splines)
 library(ISLR)
@@ -12,10 +14,11 @@ library(ISLR)
 attach(Wage)
 
 # We speified knots at ages 25, 40, and 60, which produces a spline with 6 basis functions
+# The bs() function, by default, genearte a cubic splines model
 fit.splines = lm(wage~bs(age,knots=c(25,40,60)),data=Wage)
 summary(fit.splines)
 
-# Create a age grid for prediction
+# Create an age grid for prediction
 agelims = range(age)
 age.grid=seq(agelims[1],to=agelims[2])
 
@@ -25,19 +28,33 @@ names(pred.Splines)
 
 # Plot the prediction and confidence intervals
 plot(age,wage,col="gray")
-se.bands = cbind(pred.Splines$fit+2*pred.Splines$se.fit,preds$fit-2*pred.Splines$se.fit)
+se.bands = cbind(pred.Splines$fit+2*pred.Splines$se.fit, pred.Splines$fit-2*pred.Splines$se.fit)
 lines(age.grid,pred.Splines$fit, lwd=2, col="blue") #lwd is linewidth
 matlines(age.grid,se.bands, col="blue", lty=2)  #lty shows type of line lty = 2 is used for broken lines
 
+# Note that the regular spline regression usually genearate a wider confident intervals on the two extreme regions.  
+# A Natrual Spline regression can reduce the standard errors on thesee regions.
 
-## We can change degrees of freedom and use Natural Smoothing with the ns() function
+## We can change degrees of freedom and use Natural Spline with the ns() function
+# In this example we are using df=4 to define 3 internal knots for the model.
+# By default, ns() function will pick 25th, 50th, and 75th percetiles of age, 
+# which are 33.8, 42, and 51 as the internal knots.
 fit.splines2 = lm(wage~ns(age,df=4),data=Wage)
+summary(fit.splines2)
+
+# Predict wage based on the age grid
 pred.Splines2 = predict(fit.splines2, newdata=list(age=age.grid), se=TRUE)
+
+# Plot the prediction and confidence intervals
 lines(age.grid,pred.Splines2$fit, col="red", lwd=2)
+se.bands2 = cbind(pred.Splines2$fit+2*pred.Splines2$se.fit, pred.Splines2$fit-2*pred.Splines2$se.fit)
+matlines(age.grid,se.bands2, col="red", lty=2)
+
+# As suggested by the graph, the natural spline provides a more promising prediction with 
+# narrower confident intervals on both ends of age range.
 
 
 #### Smoothing Splines ####
-plot(age,wage,col="gray")
 
 # Fitting the smooth spline by using the smooth.spline() function 
 # We use 16 degree of freedom to determines the value of lamda
@@ -50,6 +67,7 @@ names(fit.SS2)
 fit.SS2$df
 
 # Plot the two fitted model for comparison
+plot(age,wage,col="gray")
 lines(fit.SS, col="red", lwd=2)
 lines(fit.SS2, col="blue", lwd=2)
 
@@ -65,15 +83,22 @@ plot(age, wage, col="gray")
 lines(age.grid,Predict.LRL, col="blue", lwd=2)
 lines(age.grid,Predict.LRL2, col="red", lwd=2)
 
+
 ### GAMS ####
 install.packages("gam")
 library(gam)
-#s() is smoothing spline in GAMs libarary
+
+# The convenience for fitting a GAM model is that we can mix with different non-linear models
+# s() is smoothing spline in GAMs library
+# ns() is natural spline in GAMs library
+# lo() is local regression in GAMs libarary
 gam1 = gam(wage~ns(year,4)+s(age,5)+education, data=Wage)
-plot(gam1, se=TRUE, col="blue")  #not an impressive representation"
-par(mfrow=c(1,3)) #Show 3 outcomes 1 for each variable  year, age, education
-plot.gam(gam1,se=TRUE, col="red")
 summary(gam1)
+
+# Plot the prediction
+plot(gam1, se=TRUE, col="blue")  # not an impressive representation"
+par(mfrow=c(1,3)) # Show 3 outcomes 1 for each variable  year, age, education
+plot.gam(gam1,se=TRUE, col="red")
 
 
 ### ANOVA IN GAM ###
@@ -89,10 +114,13 @@ anova(gam.V1, gam.V2, gam.V3, test="F")
 PredictGam = predict(gam.V2, newdata=Wage)
 PredictGam
 
+
 ##### Adding Local regression fits by lo() models
 gam.lo = gam(wage~s(year,df=4)+lo(age,span=0.5)+education, data=Wage)
+summary(gam.lo)
 
 ##### Finally that is the time for logistic regression in GAM #####
 ## You can use I() and don't forget to set family to binomial
 gam.lr = gam(I(wage>250)~year+s(age,df=5)+education, family=binomial)
+summary(gam.lr)
 plot(gam.lr, se=TRUE, col="green")
